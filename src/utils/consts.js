e513fa9e90b64494f3d0ac4fed3764a08b435249
Marat <marat.rakhimov.2016@gmail.com>
2021-09-22 00:17:31 +0500

    third update

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,2 +1,2 @@
-export const LOGIN_ROUTE = 'https://dr1zzle1.github.io/realtime-chat/login';
-export const CHAT_ROUTE = 'https://dr1zzle1.github.io/realtime-chat/chat';
+export const LOGIN_ROUTE = '/login';
+export const CHAT_ROUTE = '/chat';
