b7c4afe3fceb44d6edbc3a60bfc599fa990688af
Marat <marat.rakhimov.2016@gmail.com>
2021-09-23 23:47:46 +0500

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,6 +5,7 @@ import { Context } from '..';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import Loader from './Loader';
 import firebase from 'firebase/compat';
+import s from './Chat.module.css';
 
 const Chat = () => {
   const { auth, firestore } = React.useContext(Context);
@@ -30,26 +31,18 @@ const Chat = () => {
   return (
     <Container>
       <Grid container style={{ height: window.innerHeight - 50 }} justify="center">
-        <div
-          style={{
-            width: '80%',
-            height: '70vh',
-            border: '1px solid gray',
-            overflowY: 'auto',
-          }}>
+        <div className={s.messages}>
           {messages.map((message) => (
             <div
+              className={s.message}
               style={{
-                margin: 10,
-                border: user.uid === message.uid ? '2px solid green' : '2px solid green',
                 marginLeft: user.uid === message.uid ? 'auto' : '10px',
-                width: 'fit-content',
               }}>
-              <Grid container>
+              <Grid container alignItems="center">
                 <Avatar src={message.photoURL} />
-                <div>{message.displayName}</div>
+                <div style={{ paddingLeft: '10px', fontSize: '18px' }}>{message.displayName}</div>
               </Grid>
-              <div>{message.text}</div>
+              <div style={{ fontSize: '18px' }}>{message.text}</div>
             </div>
           ))}
         </div>
@@ -61,13 +54,17 @@ const Chat = () => {
             width: '80%',
           }}>
           <TextField
+            style={{ marginBottom: '10px', border: ' 2px solid blue ', color: '#fff' }}
             value={value}
             onChange={(e) => setValue(e.target.value)}
             fullWidth
             maxRows={2}
-            variant="outlined"
+            placeholder="Введите ваше сообщение"
           />
-          <Button onClick={sendMessage} variant="outlined">
+          <Button
+            style={{ color: '#fff', border: ' 2px solid gray ' }}
+            onClick={sendMessage}
+            variant="outlined">
             Отправить
           </Button>
         </Grid>
